3719dd1778b4271cdd0f37200a738819a82e8e3b
Jane Venter <janeanneventer@gmail.com>
2018-09-26 03:53:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-
+function getFirstSelector(selector){
+  return document.querySelector(selector);
+}
+function nestedTarget(){
+  return document.querySelector("div#nested div.target");
+}
+function increaseRankBy(n){
+  const lis = document.querySelectorAll("ul.ranked-list");
+  for(let i = 0; i <lis.length; i++){
+    lis[i].innerHTML = parseInt(lis[i].innerHTML) + n;
+  }
+}
+function deepestChild(){
+  return document.querySelector("#grand-node div div div div");
+ // const par = document.getElementById("div#grand-node"//).querySelector("div");
+ // for(let i = 0; i< par.length; i++){
+  //  par[i];
+ // }
+}
